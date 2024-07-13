457e35f5ca4d724114159cf050cf48d57992b372
sohamshi <sohamshi@LIN-5CG119703F.corp.capgemini.com>
2024-07-13 11:29:09 +0530

    Latest commit with changes

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -4,6 +4,12 @@ import org.testng.annotations.Test;
 
 import FancodeAssignment.TodosGeneration.getUserListFancodeCity;
 
+/* 
+ * This java file is to the run the test to find out the if the users in Fancode city have completed more than 50% of their tasks
+ * GetUserList will print the results
+ * 
+ * 
+ * */
 public class TestRunner {
 
 	@Test
